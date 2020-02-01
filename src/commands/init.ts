bef85672ea766ea5b9fca5828e18b6a2a950bae2
Steve Korshakov <steve@korshakov.com>
2020-01-31 19:29:12 -0800

    feat: Add init script

---
+++ src/commands/init.ts
@@ -0,0 +1,35 @@
+import path from 'path';
+import fs from 'fs';
+import child from 'child-process-promise';
+
+export async function doInit(root: string, name: string, version: string) {
+
+    // package.json
+    console.log('Generate package.json');
+    fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify({
+        name: name,
+        version: '1.0.0',
+        devDependencies: {
+            '@types/react': '^16.9.19',
+            '@types/react-dom': '^16.9.5',
+            'react': '^16.12.0',
+            'react-dom': '^16.12.0',
+            '@haystackjs/journey': version
+        },
+        dependencies: {}
+    }, null, 2));
+
+    // tslint.json
+    console.log('Generate tslint.json');
+    fs.writeFileSync(path.join(root, 'tslint.json'), JSON.stringify({}, null, 2));
+
+    // journey.json
+    console.log('Generate journey.json');
+    fs.writeFileSync(path.join(root, 'journey.json'), JSON.stringify({ modules: [] }, null, 2));
+
+    // Install Deps
+    console.log('Instal dependencies...');
+    process.chdir(root);
+    let cp = await child.exec('yarn');
+    console.log(cp.stdout);
+}
--- src/main.ts
+++ src/main.ts
@@ -2,38 +2,49 @@ import commander from 'commander';
 import { startDevServer } from './commands/dev';
 import path from 'path';
 import { addPackage, removePackage, generateModules } from './commands/pkg';
+import { doInit } from './commands/init';
+import fs from 'fs';
+
+let version = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf-8')).version;
 
 const program = new commander.Command('journey')
-    .version('1.0.0')
+    .version(version)
     .option('--path <path>', 'Path to project');
 
 // Dev server packages
 program.command('dev <module>')
     .description('start a dev server')
     .action((pkg) => {
-        let p = (program as any).path ?  path.resolve(((program as any).path as string)) : process.cwd()
+        let p = (program as any).path ? path.resolve(((program as any).path as string)) : process.cwd()
         startDevServer(p, pkg);
+    });
+
+program.command('init <name>')
+    .description('Init package')
+    .action(async (name) => {
+        let p = (program as any).path ? path.resolve(((program as any).path as string)) : process.cwd()
+        await doInit(p, name, version);
     });
 
 // Modules
 program.command('add <module>')
     .description('Add module')
     .action((pkg) => {
-        let p = (program as any).path ?  path.resolve(((program as any).path as string)) : process.cwd()
+        let p = (program as any).path ? path.resolve(((program as any).path as string)) : process.cwd()
         addPackage(p, pkg);
     });
 
 program.command('remove <module>')
     .description('Remove module')
     .action((pkg) => {
-        let p = (program as any).path ?  path.resolve(((program as any).path as string)) : process.cwd()
+        let p = (program as any).path ? path.resolve(((program as any).path as string)) : process.cwd()
         removePackage(p, pkg);
     });
 
 program.command('refresh')
     .description('Refresh configs')
     .action(() => {
-        let p = (program as any).path ?  path.resolve(((program as any).path as string)) : process.cwd()
+        let p = (program as any).path ? path.resolve(((program as any).path as string)) : process.cwd()
         generateModules(p);
     });
 // Parse Arguments
@@ -41,5 +52,5 @@ program.parse(process.argv);
 
 // Default help
 if (program.args.length === 0) {
-  program.help();
+    program.help();
 }
